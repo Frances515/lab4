b632d108ba7157732793898129373adb24a21541
frances515 <frances.antwi-donkor@ashesi.edu.gh>
2017-10-18 14:07:26 +0100

    locator initiated

---
+++ script.js
@@ -0,0 +1,10 @@
+   
+   document.addEventListener("deviceready", onDeviceReady, false);
+    function onDeviceReady() {
+        console.log("navigator.geolocation works well");
+    }
+// function load(){
+// 	 navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+// }
+    
