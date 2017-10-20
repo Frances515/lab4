7031f82f5e93a8ac1257873b3d6cde7c59c81bf2
frances515 <frances.antwi-donkor@ashesi.edu.gh>
2017-10-21 00:27:20 +0100

    massive changes

--- script.js
+++ script.js
@@ -30,6 +30,18 @@
       	navigator.geolocation.getCurrentPosition(onSuccess, onError);
       }
 
+      function nolocate(){
+      	
+      	var uluru = {lat: -25.363, lng: 131.044};
+         map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+
+        });
+        
+
+      }
+
 
       //If the map has all the needed parameters it will load the location
       // @return latitude and longitude
